c309ab8584f59579745795c3231b1cf623109bee
mirkosorrentino <mirkosorrentino6454@gmail.com>
2023-03-07 15:25:58 +0100

    Few details with Bootstrap

--- js/script.js
+++ js/script.js
@@ -2,15 +2,18 @@
 const userName = prompt("Ciao! Benvenuto in pwd generatore. Come ti chiami?");
 console.log(userName, typeof userName);
 
-const userLastName = prompt(`Perfetto ${userName}, ho bisogno solo di poche altre informazioni. Qual è il tuo cognome?`)
+const userLastName = prompt(`Perfetto ${userName}, ho bisogno solo di poche altre informazioni. Qual è il tuo cognome?`);
 console.log(userLastName, typeof userLastName);
 
-const userFavouriteColor = prompt("Un'ultima informazione: qual è il tuo colore preferito?")
+const userFavouriteColor = prompt("Un'ultima informazione: qual è il tuo colore preferito?");
 console.log(userFavouriteColor, typeof userFavouriteColor);
 
 // elaboration
-const password = (`${userName}${userLastName}${userFavouriteColor}21`)
-console.log(password)
+const password = (`${userName}${userLastName}${userFavouriteColor}21`);
+console.log(password);
 
 // output
-document.getElementById("password").innerHTML = `${password}`
+document.getElementById("name").innerHTML = `${userName}`;
+document.getElementById("last-name").innerHTML = `${userLastName}`;
+document.getElementById("favourite-color").innerHTML = `${userFavouriteColor}`
+document.getElementById("password").innerHTML = `${password}`;
